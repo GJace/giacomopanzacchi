8f3ad98fac1bd22b829d96ec1be526eab89dd431
GJace <61358600+GJace@users.noreply.github.com>
2025-10-12 23:05:30 +0200

    changed fav icons
    changed fav icons and added web manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import "../app.css";
-	import favicon from "$lib/assets/favicon.svg";
 	import logo from "$lib/assets/asymm1024.png";
 	import github from "$lib/assets/github.svg";
 	import linkedin from "$lib/assets/linkedin.svg";
@@ -9,7 +8,15 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.ico" sizes="any" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+	<link rel="manifest" href="/site.webmanifest" />
+
+	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <div class="site">
